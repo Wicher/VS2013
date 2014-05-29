b42cc57fa984c46b84cae3f747775e9ae8badef4
Wicher <bartosz.m.zawieja@gmail.com>
2014-05-29 10:22:55 +0200

    testing...

--- WindowsFormsApplication1/EntryForm.Designer.cs
+++ WindowsFormsApplication1/EntryForm.Designer.cs
@@ -36,7 +36,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Location = new System.Drawing.Point(121, 8);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.TabIndex = 0;
